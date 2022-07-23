675322babc8a06467b2155a8bfd320f38ff58721
Delwyn007 <91654573+Delwyn007@users.noreply.github.com>
2022-07-23 15:52:34 +0530

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foodorder;
+
+/**
+ *
+ * @author DELWYN HAMILTON
+ */
+class Driver {
+    public void displaydriver(){
+        System.out.println("Delivered By: ");
+        System.out.println("Biker Babu");
+        
+    }
+}
